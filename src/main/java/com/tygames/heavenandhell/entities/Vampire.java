986b273a2198e9bcb8a5d9e65a4231071abb17bd
122Ty <71115071+122Ty@users.noreply.github.com>
2020-11-25 23:26:00 -0500

    Zane Copy
    This is zanes

--- src/main/java/com/tygames/heavenandhell/entities/Vampire.java
+++ src/main/java/com/tygames/heavenandhell/entities/Vampire.java
@@ -1,14 +1,19 @@
 package com.tygames.heavenandhell.entities;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.FleeSunGoal;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.merchant.villager.AbstractVillagerEntity;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.entity.monster.ZombiePigmanEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.SwordItem;
 import net.minecraft.world.World;
 
 public class Vampire extends MonsterEntity {
     private FleeSunGoal fleeSunGoal;
+
 
     public Vampire(EntityType<? extends MonsterEntity> type, World worldIn) {
         super(type, worldIn);
@@ -16,10 +21,20 @@ public class Vampire extends MonsterEntity {
     @Override
     protected void registerGoals(){
         super.registerGoals();
-        this.fleeSunGoal = new FleeSunGoal(this, 3);
+        this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+        this.applyEntityAI();
+    }
+    protected void applyEntityAI() {
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+        this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setCallsForHelp(ZombiePigmanEntity.class));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillagerEntity.class, false));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolemEntity.class, true));
     }
 
-    }
+
+}
 
 
 
