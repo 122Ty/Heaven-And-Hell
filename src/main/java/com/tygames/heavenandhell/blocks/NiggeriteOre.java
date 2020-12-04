195e772e6d552cf29e3bde1ac83f5db01d5cecaa
allkillernofiller <74217462+allkillernofill3r@users.noreply.github.com>
2020-12-03 23:31:26 -0500

    Quick Fix

---
+++ src/main/java/com/tygames/heavenandhell/blocks/NiggeriteOre.java
@@ -0,0 +1,11 @@
+package com.tygames.heavenandhell.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.OreBlock;
+
+public class NiggeriteOre extends OreBlock {
+    public NiggeriteOre() {
+        super(Block.Properties.from(Blocks.IRON_ORE));
+    }
+}
